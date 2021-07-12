68bc40641e45a96b6f8f05e88cec6412301bbf5f
AndreaDex <desiderio.andrea@outlook.it>
2021-07-12 17:28:05 +0200

    add some css

--- model_controller/resources/views/home.blade.php
+++ model_controller/resources/views/home.blade.php
@@ -6,18 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Model & Controller</title>
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:regular,italic,700,700italic" rel="stylesheet" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 
 <body>
-    <h1>model e controller</h1>
 
-    @foreach ($movies as $movie)
-    <div class="card">
-        <img src="{{$movie->poster}}" alt="{{$movie->title}}">
-        <h3>{{ $movie->title}}</h3>
+    <div class="gallery">
+
+        @foreach ($movies as $movie)
+        <div class="card">
+            <img src="{{$movie->poster}}" alt="{{$movie->title}}">
+            <h3>{{ $movie->title}}</h3>
+            <span>{{ $movie->release_date}}</span>
+            <div class="desc">
+                <p>{{ $movie->overview}}</p>
+            </div>
+
+        </div>
+
+        @endforeach
     </div>
-
-    @endforeach
 </body>
 
 </html>
