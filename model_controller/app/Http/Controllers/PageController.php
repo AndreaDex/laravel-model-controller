3a91eb1db988bc75edd582b47e50368639436218
AndreaDex <desiderio.andrea@outlook.it>
2021-07-12 16:40:04 +0200

    error connection

--- model_controller/app/Http/Controllers/PageController.php
+++ model_controller/app/Http/Controllers/PageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $movies = Movie::all();
+
+
+
+        return view('home', compact('movies'));
     }
 }
